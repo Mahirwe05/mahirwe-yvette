7cf2804f68294eb4a4cc01e346f15698d1884b9b
Mahirwe05 <mahirweyv123@gmail>
2025-04-30 18:45:56 +0200

    init commit

--- Advanced Stock Management System/Main.java
+++ Advanced Stock Management System/Main.java
@@ -1,7 +1,7 @@
 import java.util.Date;
 import java.util.Scanner;
 
-public class StockManagement {
+public class Main{
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
